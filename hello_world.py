c80f2a00daf95e2320b92c555cb4fe9bce16f18e
demi <h.demirbas@bssdstd.net>
2024-12-03 15:10:52 -0600

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,28 @@
+import time
+import sys
+
+class Greeting:
+    def __init__(self):
+        self.message = "Hello"
+        self.delay_time = 0.1
+
+    def print_char(self, char):
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(self.delay_time)
+
+    def print_greeting(self):
+        for char in self.message:
+            self.print_char(char)
+        print()  # Newline after the greeting
+
+    def repeat_message(self, count):
+        for _ in range(count):
+            self.print_greeting()
+
+def main():
+    greeting_instance = Greeting()
+    greeting_instance.repeat_message(1)  # Repeating just once for now (but can repeat more)
+
+if __name__ == "__main__":
+    main()
